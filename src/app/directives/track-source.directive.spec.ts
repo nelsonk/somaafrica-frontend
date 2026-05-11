6502e31c9f450466f5365af66c6ce2c687867d8f
Nelson Kanyali <nelskon@gmail.com>
2026-05-11 14:51:18 +0300

    Upgrade Angular 20 to 21 and update dependencies

--- src/app/directives/track-source.directive.spec.ts
+++ src/app/directives/track-source.directive.spec.ts
@@ -1,8 +1,10 @@
 import { TrackSourceDirective } from './track-source.directive';
+import { Router } from '@angular/router';
 
 describe('TrackSourceDirective', () => {
   it('should create an instance', () => {
-    const directive = new TrackSourceDirective();
+    const routerMock = {} as Router;
+    const directive = new TrackSourceDirective(routerMock);
     expect(directive).toBeTruthy();
   });
 });
